c4c24e2c4ced4f9ec6c4c4bbf747909e1d875a8b
nikefebr <74341372+nikefebr@users.noreply.github.com>
2022-07-05 06:01:44 +0700

    [FIX] lengkapi dokumen

--- lengkapi_dokumen/view.php
+++ lengkapi_dokumen/view.php
@@ -8,18 +8,23 @@
     header("Access-Control-Allow-Methods: POST");
     header("Content-Type: application/json; charset=UTF-8");
 
+    $input = json_decode(file_get_contents("php://input"), true);
+
+    $studentId = $input["studentId"];
+
     $conn = createDatabaseConnection();
     $query = 
-        "SELECT P.ID, P.PROGRAM_NAME, R.ID, R.SEMESTER_ID, R.STUDENT_ID, R.MBKM_PROGRAM_ID,
+        "SELECT P.ID, P.PROGRAM_NAME, R.ID, R.STUDENT_ID, R.MBKM_PROGRAM_ID,
         R.HANDPHONE, R.DESCRIPTION, R.MITRA_NAME, R.MITRA_ADDRESS, R.LINK_WEBSITE_MITRA,
-        R.STATUS, R.PRODI_ID, R.COURSE_ID, R.DPK_ID, R.REASON, R.SUGGESTION, R.DATE_START,
+        R.STATUS, R.KAPRODI_ID, R.COURSE_ID, R.DPK_ID, R.REASON, R.SUGGESTION, R.DATE_START,
         R. DATE_END, R.STATUS_KEGIATAN, R.LOGBOOK, D.CERTIFICATE
         
         FROM MBKM_REGISTRATION R 
             RIGHT JOIN MBKM_PROGRAM P ON P.ID = R.MBKM_PROGRAM_ID
             LEFT JOIN REPORT_DOCUMENT D ON R.ID = D.MBKM_REGISTRATION_ID
         
-        WHERE R.STATUS_KEGIATAN='Kegiatan Aktif' ORDER BY R.ID";
+        WHERE R.STUDENT_ID = $studentId AND R.STATUS_KEGIATAN='Kegiatan Aktif' 
+        ORDER BY R.ID";
 
     $parse_sql = oci_parse($conn, $query);
     $query_result = [];
