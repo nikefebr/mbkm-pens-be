b32fe52171123b0ccd7d9e6f0e28900d87e0355f
nikefebr <74341372+nikefebr@users.noreply.github.com>
2022-07-04 07:43:23 +0700

    [ADD] kategori program

---
+++ kategori_program/delete.php
@@ -0,0 +1,37 @@
+<?php
+include "../connection.php";
+include "../response.php";
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$conn = createDatabaseConnection();
+
+$request = file_get_contents("php://input");
+$decoded_request = json_decode($request, true);
+
+$item_id = $decoded_request['id'];
+$mbkm_program_id = $item_id;
+
+$query_child = "DELETE MBKM_REQUIREMENT WHERE MBKM_PROGRAM_ID = $item_id";
+
+$parse_sql = oci_parse($conn, $query_child);
+
+$execute = oci_execute($parse_sql) or die(oci_error());
+
+if ($execute) {
+    $query = "DELETE MBKM_KATEGORI_PROGRAM WHERE ID = $item_id";
+    $parse_sql = oci_parse($conn, $query);
+    $execute = oci_execute($parse_sql) or die(oci_error());
+    
+    if ($execute)
+        createSuccessResponse($execute, 'Item deleted successfully!!');
+    else
+        createErrorResponse('Delete data failed!');
+} else {
+    createErrorResponse('Delete requirement failed!');
+}
+?>
---
+++ kategori_program/update.php
@@ -0,0 +1,42 @@
+<?php
+include "../connection.php";
+include "../response.php";
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods", "PUT");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$conn = createDatabaseConnection();
+
+$request = file_get_contents("php://input");
+$decoded_request = json_decode($request, true);
+
+$id = $decoded_request['id'];
+$programName = $decoded_request["programName"];
+$description = $decoded_request["description"];
+
+$query = 
+    "UPDATE MBKM_KATEGORI_PROGRAM 
+    SET 
+    PROGRAM_NAME = '$programName',
+    DESCRIPTION = '$description' 
+    WHERE ID = '$id'";
+
+$parse_sql = oci_parse($conn, $query);
+
+try {
+    oci_execute($parse_sql);
+} catch (\Throwable $th) {
+    createErrorResponse('Item update failed!');
+} finally {
+    $query = "SELECT * FROM MBKM_KATEGORI_PROGRAM WHERE ID = $id";
+    $parse_sql = oci_parse($conn, $query);
+
+    oci_execute($parse_sql) or die(oci_error());
+    $query_result = oci_fetch_object($parse_sql, OCI_ASSOC+OCI_RETURN_NULLS);
+
+    createSuccessResponse($query_result, 'Item updated successfully!');
+}
+?>
