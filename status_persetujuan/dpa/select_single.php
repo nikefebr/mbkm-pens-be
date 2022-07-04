c3868c440a4e95d33513680871972cf42cf6f4a8
nikefebr <74341372+nikefebr@users.noreply.github.com>
2022-07-04 07:55:13 +0700

    [ADD] status persetujuan DPA

---
+++ status_persetujuan/dpa/select_single.php
@@ -0,0 +1,30 @@
+<?php
+include "../../connection.php";
+include "../../response.php";
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods", "GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$conn = createDatabaseConnection();
+
+$request = file_get_contents("php://input");
+$decoded_request = json_decode($request, true);
+
+$id = $decoded_request['id'];
+
+$query = "SELECT * FROM MBKM_REGISTRATION WHERE MBKM_REGISTRATION.ID = $id";
+
+$parse = oci_parse($conn, $query);
+oci_execute($parse) or die(oci_error());
+$query_result = [];
+oci_fetch_all($parse, $query_result, 0, 0, OCI_FETCHSTATEMENT_BY_ROW);
+
+if ($query_result) {
+    createSuccessResponse($query_result, 'Get item success!');
+} else {
+    createErrorResponse('Get item failed!');
+}
+?>
